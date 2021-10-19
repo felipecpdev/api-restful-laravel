471c52f90fa6efd16a20f93554f34dd498b1183f
Felipe Contreras <felipe.contreras.dev@gmail.com>
2021-10-19 16:29:55 -0300

    config store login api

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\Auth\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });*/
-
+Route::post('login', [LoginController::class, 'store']);
 Route::post('register', [RegisterController::class, 'store'])->name('api.v1.register');
 
 /*Route::get('categories', [CategoryController::class, 'index'])->name('api.v1.categories.index');
